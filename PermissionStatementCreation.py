f69f5a04e290c4c30db5b9c3a5f8e0111733dd69
abdelaal251 <ahmedabdelaal4563@gmail.com>
2024-06-21 19:17:00 +0300

    deliverables preparation phase 1

---
+++ PermissionStatementCreation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# Load the CSV file
+file_path = 'user permisssions.csv'
+permissions_df = pd.read_csv(file_path)
+
+# Function to generate SQL statements
+def generate_sql_statements(permissions_df):
+    sql_statements = []
+
+    for index, row in permissions_df.iterrows():
+        procedure_function = row['Procedure / Function']
+        
+        for user_role in ['Admin1', 'TrainingManager1', 'Instructor1', 'Student1']:
+            permission = row[user_role]
+            if permission == 'GRANT':
+                sql_statements.append(f"GRANT EXECUTE ON OBJECT::{procedure_function} TO {user_role};")
+            elif permission == 'DENY':
+                sql_statements.append(f"DENY EXECUTE ON OBJECT::{procedure_function} TO {user_role};")
+
+    return sql_statements
+
+# Generate SQL statements
+sql_statements = generate_sql_statements(permissions_df)
+
+# Print SQL statements
+for statement in sql_statements:
+    print(statement)
--- UsersCreationWIthDiffenetRoles.sql
+++ UsersCreationWIthDiffenetRoles.sql
@@ -5,9 +5,26 @@ CREATE LOGIN Instructor1 WITH PASSWORD = 'P@ssw0rd';
 CREATE LOGIN Student1 WITH PASSWORD = 'P@ssw0rd';
 
 -- Create a database users
-USE ExaminationSystem;
+USE ExSysTest;
 CREATE USER Admin1 FOR LOGIN Admin1;
 CREATE USER TrainingManager1 FOR LOGIN TrainingManager1;
 CREATE USER Instructor1 FOR LOGIN Instructor1;
 CREATE USER Student1 FOR LOGIN Student1;
 
+
+
+GRANT EXECUTE ON OBJECT::RegisterUser TO Admin1;
+GRANT EXECUTE ON OBJECT::AuthenticateUser TO Admin1;
+GRANT EXECUTE ON OBJECT::UpdateUserInfo TO Admin1;
+GRANT EXECUTE ON OBJECT::DeleteUser TO Admin1;
+GRANT EXECUTE ON OBJECT::AddNewCourse TO Admin1;
+GRANT EXECUTE ON OBJECT::AssignInstructorToCourse TO Admin1;
+GRANT EXECUTE ON OBJECT::UpdateCourseDetails TO Admin1;
+GRANT EXECUTE ON OBJECT::DeleteCourse TO Admin1;
+GRANT EXECUTE ON OBJECT::CreateExam TO Admin1;
+GRANT EXECUTE ON OBJECT::AddQuestionsToExam TO Admin1;
+GRANT EXECUTE ON OBJECT::RecordStudentAnswers TO Admin1;
+GRANT EXECUTE ON OBJECT::UpdateExamDetails TO Admin1;
+GRANT EXECUTE ON OBJECT::DeleteExam TO Admin1;
+
+exec RegisterUser 
