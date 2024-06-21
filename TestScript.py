5ad6742130782d25bb7fa019fef646c9ad9b3185
abdelaal251 <ahmedabdelaal4563@gmail.com>
2024-06-21 19:21:32 +0300

    Squashed commit of the following:
    commit 0f777c768797d12f5d8e96449330d53daf0c0838
    Author: abdelaal251 <ahmedabdelaal4563@gmail.com>
    Date:   Fri Jun 21 19:20:39 2024 +0300
    
        folder renamed
    
    commit 55568ab8d2929d4383f4b7a8f3af1cee87e15a34
    Author: abdelaal251 <ahmedabdelaal4563@gmail.com>
    Date:   Fri Jun 21 19:17:52 2024 +0300
    
        Squashed commit of the following:
    
        commit 1e09a9e582130478e70876ee8c31fbfc3172028d
        Author: Noha Gamal <120256757+NohaGamal118@users.noreply.github.com>
        Date:   Fri Jun 21 18:43:27 2024 +0300
    
            Noha.sql
    
        commit 562cca193add91b6d812d6f9adff5edb0b286a6a
        Author: Ahmed Mohamed Maher Sayed <ahmedmaher.maher61@gmail.com>
        Date:   Fri Jun 21 18:13:07 2024 +0300
    
            Alter Some Views
    
        commit b4a7cc4eb01a0ad07a94facd3117e15d18d450c5
        Author: Noha Gamal <120256757+NohaGamal118@users.noreply.github.com>
        Date:   Fri Jun 21 17:30:54 2024 +0300
    
            Noha.sql
    
        commit ce0a83a2370a0d6ef310660e738810663e1ad629
        Author: Noha Gamal <120256757+NohaGamal118@users.noreply.github.com>
        Date:   Fri Jun 21 16:30:12 2024 +0300
    
            Noha.sql
    
    commit f69f5a04e290c4c30db5b9c3a5f8e0111733dd69
    Author: abdelaal251 <ahmedabdelaal4563@gmail.com>
    Date:   Fri Jun 21 19:17:00 2024 +0300
    
        deliverables preparation phase 1

--- TestScript.py
+++ TestScript.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Database connection details
 server = '77.37.120.167,1433'
-database = 'ExSysTest'
+database = 'ExSys'
 
 # User credentials
 credentials = {
@@ -15,19 +15,30 @@ credentials = {
 
 # Procedures and Functions to test
 procedures_functions = [
-    'RegisterUser',
-    'AuthenticateUser',
-    'UpdateUserInfo',
-    'DeleteUser',
-    'AddNewCourse',
-    'AssignInstructorToCourse',
-    'UpdateCourseDetails',
-    'DeleteCourse',
-    'CreateExam',
-    'AddQuestionsToExam',
-    'RecordStudentAnswers',
-    'UpdateExamDetails',
-    'DeleteExam'
+    '[dbo].[AddNewBranch]',
+    '[dbo].[AddNewCourse]',
+    '[dbo].[AddNewIntake]',
+    '[dbo].[AddNewTrack]',
+    '[dbo].[AddQuestionsToExam]',
+    '[dbo].[AssignInstructorToCourse]',
+    '[dbo].[AssignStudentToExam]',
+    '[dbo].[CreateExam]',
+    '[dbo].[DeleteBranch]',
+    '[dbo].[DeleteCourse]',
+    '[dbo].[DeleteExam]',
+    '[dbo].[DeleteStudent]',
+    '[dbo].[DeleteUser]',
+    '[dbo].[EditBranch]',
+    '[dbo].[EditIntake]',
+    '[dbo].[EditTrack]',
+    '[dbo].[RecordStudentAnswers]',
+    '[dbo].[RegisterStudent]',
+    '[dbo].[RegisterUser]',
+    '[dbo].[UpdateCourseDetails]',
+    '[dbo].[UpdateExamDetails]',
+    '[dbo].[UpdateStudentDetails]',
+    '[dbo].[UpdateUserInfo]',
+    '[dbo].[AuthenticateUser]'
 ]
 
 # Log results
@@ -35,32 +46,54 @@ results = []
 
 def test_procedure_function(cursor, name):
     try:
-        if name == 'RegisterUser':
-            cursor.execute("{CALL RegisterUser('John', 'Doe', 'john.doe@example.com', 'Student', 'password123')}")
-        elif name == 'AuthenticateUser':
+        if name == '[dbo].[AddNewBranch]':
+            cursor.execute("{CALL dbo.AddNewBranch('Software Engineering', 1)}")
+        elif name == '[dbo].[AddNewCourse]':
+            cursor.execute("{CALL dbo.AddNewCourse('Math 101', 'Basic Math Course', 100, 60)}")
+        elif name == '[dbo].[AddNewIntake]':
+            cursor.execute("{CALL dbo.AddNewIntake('Fall 2024', '2024-09-01', '2024-12-31', 1)}")
+        elif name == '[dbo].[AddNewTrack]':
+            cursor.execute("{CALL dbo.AddNewTrack('Artificial Intelligence', 1)}")
+        elif name == '[dbo].[AddQuestionsToExam]':
+            cursor.execute("{CALL dbo.AddQuestionsToExam(1, 1, 10)}")
+        elif name == '[dbo].[AssignInstructorToCourse]':
+            cursor.execute("{CALL dbo.AssignInstructorToCourse(1, 1, 2024)}")
+        elif name == '[dbo].[AssignStudentToExam]':
+            cursor.execute("{CALL dbo.AssignStudentToExam(1, 1, '2024-06-15 10:00:00', '2024-06-15 12:00:00')}")
+        elif name == '[dbo].[CreateExam]':
+            cursor.execute("{CALL dbo.CreateExam('Final', '2024-06-15 10:00:00', '2024-06-15 12:00:00', 'Corrective', 1, 1)}")
+        elif name == '[dbo].[DeleteBranch]':
+            cursor.execute("{CALL dbo.DeleteBranch(1)}")
+        elif name == '[dbo].[DeleteCourse]':
+            cursor.execute("{CALL dbo.DeleteCourse(1)}")
+        elif name == '[dbo].[DeleteExam]':
+            cursor.execute("{CALL dbo.DeleteExam(1)}")
+        elif name == '[dbo].[DeleteStudent]':
+            cursor.execute("{CALL dbo.DeleteStudent(1)}")
+        elif name == '[dbo].[DeleteUser]':
+            cursor.execute("{CALL dbo.DeleteUser(1)}")
+        elif name == '[dbo].[EditBranch]':
+            cursor.execute("{CALL dbo.EditBranch(1, 'Network Engineering', 1)}")
+        elif name == '[dbo].[EditIntake]':
+            cursor.execute("{CALL dbo.EditIntake(1, 'Spring 2024', '2024-01-01', '2024-04-30', 1)}")
+        elif name == '[dbo].[EditTrack]':
+            cursor.execute("{CALL dbo.EditTrack(1, 'Cybersecurity', 1)}")
+        elif name == '[dbo].[RecordStudentAnswers]':
+            cursor.execute("{CALL dbo.RecordStudentAnswers(1, 1, 'Answer text')}")
+        elif name == '[dbo].[RegisterStudent]':
+            cursor.execute("{CALL dbo.RegisterStudent(1)}")
+        elif name == '[dbo].[RegisterUser]':
+            cursor.execute("{CALL dbo.RegisterUser('John', 'Doe', 'john.doe@example.com', 'Student', 'FullTime', 'password123')}")
+        elif name == '[dbo].[UpdateCourseDetails]':
+            cursor.execute("{CALL dbo.UpdateCourseDetails(1, 'Math 102', 'Intermediate Math Course', 100, 60)}")
+        elif name == '[dbo].[UpdateExamDetails]':
+            cursor.execute("{CALL dbo.UpdateExamDetails(1, 'Midterm', '2024-06-20 10:00:00', '2024-06-20 12:00:00', 'ClosedBook')}")
+        elif name == '[dbo].[UpdateStudentDetails]':
+            cursor.execute("{CALL dbo.UpdateStudentDetails(1, 'Updated Details')}")
+        elif name == '[dbo].[UpdateUserInfo]':
+            cursor.execute("{CALL dbo.UpdateUserInfo(1, 'John', 'Doe', 'john.doe@example.com', 'Student', 'PartTime')}")
+        elif name == '[dbo].[AuthenticateUser]':
             cursor.execute("SELECT dbo.AuthenticateUser('john.doe@example.com', 'password123')")
-        elif name == 'UpdateUserInfo':
-            cursor.execute("{CALL UpdateUserInfo(1, 'John', 'Doe', 'john.doe@example.com', 'Student', 'PartTime')}")
-        elif name == 'DeleteUser':
-            cursor.execute("{CALL DeleteUser(1)}")
-        elif name == 'AddNewCourse':
-            cursor.execute("{CALL AddNewCourse('Math 101', 'Basic Math Course', 100, 60)}")
-        elif name == 'AssignInstructorToCourse':
-            cursor.execute("{CALL AssignInstructorToCourse(1, 1, 2024)}")
-        elif name == 'UpdateCourseDetails':
-            cursor.execute("{CALL UpdateCourseDetails(1, 'Math 102', 'Intermediate Math Course', 100, 60)}")
-        elif name == 'DeleteCourse':
-            cursor.execute("{CALL DeleteCourse(1)}")
-        elif name == 'CreateExam':
-            cursor.execute("{CALL CreateExam('Final', '2024-06-15 10:00:00', '2024-06-15 12:00:00', 'OpenBook', 1, 1)}")
-        elif name == 'AddQuestionsToExam':
-            cursor.execute("{CALL AddQuestionsToExam(1, 1, 10)}")
-        elif name == 'RecordStudentAnswers':
-            cursor.execute("{CALL RecordStudentAnswers(1, 1, 'Answer text')}")
-        elif name == 'UpdateExamDetails':
-            cursor.execute("{CALL UpdateExamDetails(1, 'Midterm', '2024-06-20 10:00:00', '2024-06-20 12:00:00', 'ClosedBook')}")
-        elif name == 'DeleteExam':
-            cursor.execute("{CALL DeleteExam(1)}")
         cursor.commit()
         return 'pass', ''
     except Exception as e:
